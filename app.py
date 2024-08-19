e828885283ee715591f79021962b3c35b0c13689
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-08-19 18:58:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,64 +31,77 @@ def home():
         key_data["key"] = generate_key()
         key_data["timestamp"] = time.time()
     return f'''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Access Key</title>
-        <style>
-            body {{
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-                position: relative;
-            }}
-            .content {{
-                text-align: center;
-            }}
-            .author {{
-                position: absolute;
-                top: 10px;
-                left: 10px;
-                color: #000;
-                font-size: 18px;
-            }}
-            .banner-telegram {{
-                position: absolute;
-                top: 10px;
-                right: 10px;
-                background-color: #0088cc;
-                padding: 10px;
-                border-radius: 5px;
-                box-shadow: 0 2px 4px rgba(0,0,0,0.2);
-            }}
-            .banner-telegram a {{
-                color: #ffcc00;
-                text-decoration: none;
-                font-weight: bold;
-            }}
-        </style>
-    </head>
-    <body>
-        <div class="author">Autor = Keno Venas</div>
-        <div class="banner-telegram">
-            <a href="https://t.me/+Mns6IsONSxliZDkx" target="_blank">Grupo do Telegram</a>
-        </div>
-        <div class="content">
-            <h1>Access Key</h1>
-            <p>{key_data["key"]}</p>
-        </div>
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Access Key</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            position: relative;
+        }
+        .content {
+            text-align: center;
+        }
+        .author {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            color: #000;
+            font-size: 18px;
+        }
+        .banner-telegram {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            background-color: #0088cc;
+            padding: 10px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+        }
+        .banner-telegram a {
+            color: #ffcc00;
+            text-decoration: none;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="author">Autor = Keno Venas</div>
+    <div class="banner-telegram">
+        <a href="https://t.me/+Mns6IsONSxliZDkx" target="_blank">Grupo do Telegram</a>
+    </div>
+    <div class="content">
+        <h1>Access Key</h1>
+        <p>{key_data["key"]}</p>
+    </div>
 
-        <!-- Script da Hydro -->
-        <script id="hydro_config" type="text/javascript">
-            window.Hydro_tagId = "ab51bfd4-d078-4c04-a17b-ccfcfe865175";
-        </script>
-        <script id="hydro_script" src="https://track.hydro.online/"></script>
-    </body>
-    </html>
+    <!-- Script da Hydro -->
+    <script id="hydro_config" type="text/javascript">
+        window.Hydro_tagId = "ab51bfd4-d078-4c04-a17b-ccfcfe865175";
+    </script>
+    <script id="hydro_script" src="https://track.hydro.online/"></script>
+
+    <!-- Novo Script Adicionado -->
+    <script type="text/javascript">
+        atOptions = {
+            'key' : '78713e6d4e36d5a549e9864674183de6',
+            'format' : 'iframe',
+            'height' : 90,
+            'width' : 728,
+            'params' : {}
+        };
+    </script>
+    <script type="text/javascript" src="//www.topcreativeformat.com/78713e6d4e36d5a549e9864674183de6/invoke.js"></script>
+</body>
+</html>
+
     '''
 
 @app.route('/validate', methods=['POST'])
