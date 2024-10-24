4036b9a1a29e02bf2d809be545d57929d512942f
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-10-24 20:14:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ allowed_users = {
                  "ok3286",
                  "ok1390",
                  "zr1",
-                 "nbsbt",
                  "mxchk",
                  "pdrrm",
                  "hll",
