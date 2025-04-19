c49f8ae42a70c9a00464f2ebc99b8d4ac9b312e7
kenovenas <136008924+kenovenas@users.noreply.github.com>
2025-04-19 16:49:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from telethon.sync import TelegramClient
 
-# Insira diretamente aqui:
 api_id = 20225004
 api_hash = '8f4c78e858658cd2aa21967a087bf819'
 
-canal_origem = 2656975250
-canal_destino = 2590813877
-
 with TelegramClient('session', api_id, api_hash) as client:
-    print("Conectado com sucesso!")
-    for message in client.iter_messages(canal_origem, limit=5):
-        if message.text:
-            client.send_message(canal_destino, message.text)
-            print(f"Mensagem enviada: {message.text[:30]}...")
+    print("Login concluído e sessão criada.")
